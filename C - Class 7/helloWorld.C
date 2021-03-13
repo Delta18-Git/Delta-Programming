ca31b234f2185dba0dcf48bfe52a794b88bacaff
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2021-03-13 17:01:57 +0530

    Class 7 - Turbo C++
    Dump - Unknown Dates

---
+++ C - Class 7/helloWorld.C
@@ -0,0 +1,10 @@
+//VS00
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    clrscr();
+    //The 0th program by VGD and SP
+    printf("Hello world.\nHello Vinaayak and Suraj.");
+    getch();
+}
---
+++ C - Class 7/nameclassSection.C
@@ -0,0 +1,10 @@
+//VS01
+#include <stdio.h>
+#include <conio.h>
+//A program to display name, class and section.
+void main()
+{
+    clrscr();
+    printf("Vinaayak G Dasika\tSuraj Premkumar\nGrade 7\t\t\tGrade 7\nVaiduryam\t\tVaiduryam");
+    getch();
+}
