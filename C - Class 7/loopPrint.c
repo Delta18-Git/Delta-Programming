1dce9e4d3d62d28f35333b5e1b986619e07f828c
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2021-03-13 17:25:54 +0530

    Class 7 - 28/02/2019

---
+++ C - Class 7/loopPrint.c
@@ -0,0 +1,12 @@
+//VS14
+#include <stdio.h>
+#include <conio.h>
+//By Vinaayak G Dasika and Suraj Premkumar
+//On 28/02/19
+void main()
+{
+    clrscr();
+    for (int x = 10; x <= 50; x++)
+        printf("\t\n %d", x);
+    getch();
+}
